5b32a5c195254a8f5eccc419d06eec145ff740bb
jinzhiyuan0327 <jinzhiyuan0327@163.com>
2026-08-04 18:20:22 +0800

    style: restore project logo on mobile headers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "novora-shell-v2.7.1-pwa-20260804-4";
-const RUNTIME_CACHE = "novora-runtime-v2.7.1-pwa-20260804-4";
+const SHELL_CACHE = "novora-shell-v2.7.1-pwa-20260804-5";
+const RUNTIME_CACHE = "novora-runtime-v2.7.1-pwa-20260804-5";
 const SHELL_ASSETS = ["/", "/index.html", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
